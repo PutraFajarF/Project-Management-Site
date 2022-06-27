c29a8c489d7eaea2f4945eafa7a3f1186ce1ccec
Putra Fajar <putrautalls@gmail.com>
2022-06-28 00:27:47 +0700

    install firebase@8.5, setting up firebase and add Config.js to ini firebase and service

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBp2BsztTKvg9aYZ8b1snt8xHNXCsw4kMs",
+  authDomain: "thepromansite-73c3d.firebaseapp.com",
+  projectId: "thepromansite-73c3d",
+  storageBucket: "thepromansite-73c3d.appspot.com",
+  messagingSenderId: "957630370747",
+  appId: "1:957630370747:web:f1600c3a2f680cc2d9aa9e"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig);
+
+// init service
+const projectFirestore = firebase.firestore();
+const projectAuth = firebase.auth();
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp;
+
+export { projectFirestore, projectAuth, timestamp };
