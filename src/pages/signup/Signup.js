006d98b529830195bff5eb6432616cd7fc219bbb
Putra Fajar <putrautalls@gmail.com>
2022-06-28 02:15:20 +0700

    create form, input and label in Signup.js page and styling it

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,13 +1,55 @@
+import { useState } from 'react';
+
 // styles
 import './Signup.css';
 
 
 const Signup = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState(null);
+
   return (
-    <div>
-      Signup
-    </div>
-  )
+    <form className='auth-form'>
+      <h2>Sign Up</h2>
+      <label>
+        <span>Display Name:</span>
+        <input
+          required
+          type='text'
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>Email:</span>
+        <input
+          required
+          type='email'
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>Password:</span>
+        <input
+          required
+          type='password'
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>Profile Thumbail:</span>
+        <input
+          required
+          type='file'
+        />
+      </label>
+      <button className='btn'>Sign Up</button>
+    </form>
+  );
 };
 
 export default Signup;
