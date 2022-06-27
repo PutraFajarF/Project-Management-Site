435153ee96bd8b6dd9dfa3698fcc712422941881
Putra Fajar <putrautalls@gmail.com>
2022-06-28 02:49:51 +0700

    setup Firebase Storage and init service it in config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/storage';
 
 const firebaseConfig = {
   apiKey: "AIzaSyBp2BsztTKvg9aYZ8b1snt8xHNXCsw4kMs",
@@ -17,8 +18,9 @@ firebase.initializeApp(firebaseConfig);
 // init service
 const projectFirestore = firebase.firestore();
 const projectAuth = firebase.auth();
+const projectStorage = firebase.storage();
 
 // timestamp
 const timestamp = firebase.firestore.Timestamp;
 
-export { projectFirestore, projectAuth, timestamp };
+export { projectFirestore, projectAuth, projectStorage, timestamp };
