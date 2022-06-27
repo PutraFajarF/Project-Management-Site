3e642499b524e734c309ca3580cb5581d217f5d1
Putra Fajar <putrautalls@gmail.com>
2022-06-28 01:33:19 +0700

    adding Navbar component and styling it then insert it in App Component

--- src/App.js
+++ src/App.js
@@ -9,12 +9,14 @@ import Signup from './pages/signup/Signup';
 import Login from './pages/login/Login';
 import Create from './pages/create/Create';
 import Project from './pages/project/Project';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className='App'>
       <BrowserRouter>
         <div className='container'>
+          <Navbar />
           <Routes>
             <Route index path='/' element={<Dashboard />} />
             <Route path='create' element={<Create />} />
