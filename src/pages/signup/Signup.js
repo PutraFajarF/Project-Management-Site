de2e3b01f44faef23a4272d488229c3c9914cdb3
Putra Fajar <putrautalls@gmail.com>
2022-06-28 17:55:03 +0700

    adding update online status configuration to false in useLogout.js and apply it in Navbar Component

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -78,7 +78,7 @@ const Signup = () => {
         {thumbnailError && <div className='error'>{thumbnailError}</div>}
       </label>
       {!isPending && <button className='btn'>Sign Up</button>}
-      {isPending && <button className='btn' disabled>Loading</button>}
+      {isPending && <button className='btn' disabled>Loading..</button>}
       {error && <div className='error'>{error}</div>}
     </form>
   );
