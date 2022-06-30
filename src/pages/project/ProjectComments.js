167c1ca37e9e90077443f1369435473c114e64b3
Putra Fajar <putrautalls@gmail.com>
2022-06-30 20:19:54 +0700

    show comments card beside ProjectSummary.js and styling it

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -3,6 +3,9 @@ import { timestamp } from "../../firebase/config";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { useFirestore } from "../../hooks/useFirestore";
 
+// component
+import Avatar from "../../components/Avatar";
+
 const ProjectComments = ({ project }) => {
   const [newComment, setNewComment] = useState('');
   const { user } = useAuthContext();
@@ -28,6 +31,23 @@ const ProjectComments = ({ project }) => {
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
+      <ul>
+        {project.comments.length > 0 && project.comments.map(comment => (
+          <li key={comment.id}>
+            <div className="comment-author">
+              <Avatar src={comment.photoURL} />
+              <p>{comment.displayName}</p>
+            </div>
+            <div className="comment-date">
+              <p>Date Here</p>
+            </div>
+            <div className="comment-content">
+              <p>{comment.content}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
+
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
