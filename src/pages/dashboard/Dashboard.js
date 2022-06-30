622f2d533913223c381259e195872bc83cfa30b9
Putra Fajar <putrautalls@gmail.com>
2022-06-30 17:21:20 +0700

    create ProjectList.js components for display new project in Dashboard page

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,10 +1,18 @@
+import { useCollection } from '../../hooks/useCollection';
+
+// components
+import ProjectList from '../../components/ProjectList';
+
 // styles
 import './Dashboard.css';
 
 const Dashboard = () => {
+  const { documents, error } = useCollection('projects');
   return(
     <div>
-      Dashboard
+      <h2 className='page-title'>Dashboard</h2>
+      {error && <p className='error'>{error}</p>}
+      {documents && <ProjectList projects={documents}/>}
     </div>
   )
 };
